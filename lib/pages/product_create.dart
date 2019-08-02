a43c065b5fbc4f23d17282aaf2154cdb622eaac7
Amalia <amalia.arstamyan.s@tumo.org>
2019-08-02 10:31:29 +0400

    trigger widget via condations

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 
 class ProductCreatePage extends StatefulWidget {
   @override
@@ -11,45 +13,101 @@ class ProductCreatePageState extends State<ProductCreatePage> {
   String titleValue;
   String descriptionValue;
   double priceValue;
+  String type;
+  String expirationDate;
+
+  final _formKey = GlobalKey<FormState>();
+  DateTime date;
+
+  final format = DateFormat("EEEE, MMMM d, yyyy");
+
+  DropdownButton<String> _dropdown() => DropdownButton(
+        hint: Text('Type'),
+        items: [
+          DropdownMenuItem(
+            child: Text('New'),
+            value: 'New',
+          ),
+          DropdownMenuItem(
+            child: Text('Out date'),
+            value: 'Out date',
+          ),
+        ],
+        onChanged: (value) {
+          setState(() {
+            type = value;
+          });
+        },
+        value: type,
+
+
+      );
+  Widget _buildRelationTile() {
+    if ( type == "New" ) {
+      return Container();
+    } else {
+      return DateTimePickerFormField(
+          format: format,
+          editable: false,
+          dateOnly: true,
+          decoration: InputDecoration(
+              labelText: 'Date', hasFloatingPlaceholder: false),
+          onChanged: (dt) {
+            date = dt;
+            setState(() {
+              expirationDate = date.toString();
+              print(type);
+              print(expirationDate);
+
+            });
+          });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      key: _formKey,
       margin: EdgeInsets.all(15.0),
-      child: ListView(
-        children: <Widget>[
-          TextField(
-            onChanged: (String value) {
-              setState(() {
-                titleValue = value;
-              });
-            },
-            decoration: InputDecoration(
-              labelText: 'product title',
+      child: Form(
+        child: ListView(
+          children: <Widget>[
+            TextFormField(
+              onSaved: (String value) {
+                setState(() {
+                  titleValue = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'product title',
+              ),
             ),
-          ),
-          TextField(
-            onChanged: (String value) {
-              setState(() {
-                descriptionValue = value;
-              });
-            },
-            decoration: InputDecoration(
-              labelText: 'product description',
+            TextFormField(
+              onSaved: (String value) {
+                setState(() {
+                  descriptionValue = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'product description',
+              ),
             ),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            onChanged: (String value) {
-              setState(() {
-                priceValue = double.parse(value);
-              });
-            },
-            decoration: InputDecoration(
-              labelText: 'product price',
+            TextFormField(
+              keyboardType: TextInputType.number,
+              onSaved: (String value) {
+                setState(() {
+                  priceValue = double.parse(value);
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'product price',
+              ),
             ),
-          )
-        ],
+            _dropdown(),
+            _buildRelationTile()
+
+          ],
+        ),
       ),
     );
   }
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:icone_1/pages/product_create.dart';
 
 class ProductListPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Center(
